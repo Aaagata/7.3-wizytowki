ccd552efd6f7e5f9c0c133391f48d12f884f1769
Agata Dunaj <a.dunaj@yahoo.co.uk>
2021-02-26 06:38:00 +0100

    add final changes

--- ksiazka_adresowa.py
+++ ksiazka_adresowa.py
@@ -4,11 +4,15 @@ class BaseContact:
        self.surname = surname
        self.private_mail = private_mail  
     def __str__(self):
-        return f"Kontaktuję się z {self.name} {self.surname} i piszę e-mail na adres: {self.private_mail}"
-    @property
-    def label_lenght(self):
-        self.label_lenght = len(self.name) + len(self.surname)
-        return self.label_lenght    
+        return f"{self.name} {self.surname}: {self.private_mail}"
+    
+    # @property
+    # def label_lenght(self):
+    #     return self.label_lenght    
+    # @label_lenght.setter
+    def fullname(self):
+        return len(self.name + ' ' + self.surname)
+
 def fake_data():
     from faker import Faker
     fake = Faker()
@@ -29,14 +33,14 @@ class BusinessContact(BaseContact):
         self.company = company
         self.job_title = job_title
         self.company_phone = company_phone
-    def contact():
-        print(f"Dzwonię do {self.name} {self.surname} z firmy {self.company} pod numer telefonu: {self.company_phone}")
+    def contact(self):
+        return f"Dzwonię do {self.name} {self.surname} z firmy {self.company} pod numer telefonu: {self.company_phone}"
 def fake_data2():
     from faker import Faker
     fake = Faker()
     name, surname = fake.name().split()
     return BusinessContact(name=name, surname=surname, private_mail=fake.email(), company=fake.company(), job_title=fake.job(), company_phone=fake.ssn())
-p1 = BusinessContact("Alicja", "Walczak", "aa@o2.pl", "Budimex", "Dyrektor Finansowy", "+48221231212")
+p1 = BusinessContact(name="Alicja", surname="Walczak", private_mail="aa@o2.pl", company="Budimex", job_title="Dyrektor Finansowy", company_phone="+48221231212")
 p2 = BusinessContact("Olga", "Lipińska", "oo@o2.pl", "TVP", "Reżyser", "+48226424242")
 p3 = BusinessContact("Roman","Bratny", "rr@o2.pl", "PGE","Główny Księgowy", "+48228832211")
 p4 = BusinessContact("Marek","Waszczuk", "mm@o2.pl", "SztormGrupa","CEO", "+48606332211")
@@ -44,4 +48,14 @@ p5 = BusinessContact("Bjork", "Johansson", "bb@o2.pl", "IKEA", "Account Manager"
 people_list = [p1, p2, p3, p4, p5]
 people_list.append(fake_data2())
 for i in people_list:
-    print(i)
+    print(i.contact())
+print(p1.fullname())
+def create_contacts(rodzaj, ilosc):
+    if rodzaj == BusinessContact:
+        return [fake_data2() for _ in range(ilosc)]
+    elif rodzaj == BaseContact:
+        return [fake_data() for _ in range(ilosc)]
+for contact in create_contacts(BusinessContact, 15):
+    print(contact)
+
+
